0c7b36575771b031a518bb920f5812c4f9855019
Joann Briggs <joannbriggs@Joanns-MacBook-Pro.local>
2019-11-08 10:28:49 -0500

    commit before git fix

--- models/seed.js
+++ models/seed.js
@@ -9,7 +9,8 @@ module.exports = [
     breed: "thoroughbred/appaloosa",
     color: "chesnut with spots",
     height: "15.2",
-    img: "http://bluelips.com/prod_images_large/bones1.jpg",
+    img:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/25289195_10155969807984190_6967816834374220643_n.jpg?_nc_cat=105&_nc_oc=AQms2_8p2uf0ibfgGFj4rTXQ7aixwTr3L6bmS2Oz6VJgVF-f0zEzhV2IRD61q3lnYUg&_nc_ht=scontent-lga3-1.xx&oh=0e1dfc268b52abd4d8cb6b550cb5c815&oe=5E53A130",
     supplements: "1 lb red cell 2x daily. Prevacox 1x daily",
     forSale: false,
     price: 0
@@ -24,7 +25,8 @@ module.exports = [
     breed: "Quarter Horse",
     color: "Buckskin",
     height: "15h",
-    img: "http://bluelips.com/prod_images_large/bones1.jpg",
+    img:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/43255918_10213091894997507_475443035602157568_o.jpg?_nc_cat=102&_nc_oc=AQmmgpcyYlVZBKqsQEo_PBHUquITv4ZRb327xGbTcW5Z96tWRweogP8X8YJfNPwPs5g&_nc_ht=scontent-lga3-1.xx&oh=72ac2beea6fb0be1bc0116e5d38ee136&oe=5E58DCB4",
     supplements: "1 lb safechoice - 1/2 cup horsesheen - 1 cup aloe",
     forSale: false,
     price: 0
@@ -39,7 +41,8 @@ module.exports = [
     breed: "Azteca (quarter horse/andelusion)",
     color: "black",
     height: "15h",
-    img: "http://bluelips.com/prod_images_large/bones1.jpg",
+    img:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/72941808_10220267475062497_6694144522990911488_o.jpg?_nc_cat=107&_nc_oc=AQmBUlkQcuzZ1Bx-bZPvZZukiXZY4dx8_Hko-MB5Tf6W_9A1VxSWmMz8mQOcxKk54bk&_nc_ht=scontent-lga3-1.xx&oh=647dfb9b9f7624259cdf89180ae1b9d6&oe=5E603933",
     supplements: "1/2 lb Red Cell",
     forSale: false,
     price: 0
@@ -53,7 +56,8 @@ module.exports = [
     breed: "Grade",
     color: "bay",
     height: "15h",
-    img: "http://bluelips.com/prod_images_large/bones1.jpg",
+    img:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/73039935_2999670350050156_5474069375055036416_n.jpg?_nc_cat=101&_nc_oc=AQkGN-orH_E31VrNS6GJrA2UaKNrAZ8uoRy3KR8Km4th6gWw9dEGutbRuYznDbZl_D8&_nc_ht=scontent-lga3-1.xx&oh=bf68bd11b149ad67aa7aaf2ccddf17b8&oe=5E54BCE5",
     supplements:
       "1lb SafeChoice, 1/2 cup horsesheen, 1/2 cup milk thistle, 1/4 cup magnesium",
     forSale: false,
@@ -69,7 +73,8 @@ module.exports = [
     breed: "OTTD",
     color: "Grey",
     height: "16h",
-    img: "http://bluelips.com/prod_images_large/bones1.jpg",
+    img:
+      "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/71855105_2278360805626220_8915669661962469376_o.jpg?_nc_cat=108&_nc_oc=AQn-sMJ3UxTKaQ9-QIFXmcCAVB-l-3zPu4Z8RRp0JBvx_58VNo8OaorHa5O2NbK1IXc&_nc_ht=scontent-lga3-1.xx&oh=6826fc0507019668cbd6aa568c7ce8d2&oe=5E17B24A",
     supplements: "1/2 lb Red Cell",
     forSale: false,
     price: 0
